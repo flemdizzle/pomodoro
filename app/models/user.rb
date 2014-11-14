d6f9e1782f0b9a1a63e2e8fd28ffb0c9161957db
behrgood <behrgood@gmail.com>
2014-11-14 09:58:01 -0500

    Add has_many through: associations on user, team

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
    has_many :tasks, dependent: :destroy
-   has_many :teams
+   has_many :teams, through: :members
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
