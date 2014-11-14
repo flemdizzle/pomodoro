d4e543274c8abcd7b210ba6103b9d15ea7e91737
lstamato <lydia.stamato@gmail.com>
2014-11-14 11:15:48 -0500

    Add attr_accessible to user.rb to make app work on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Trying to get paths working on heroku
+  # http://stackoverflow.com/questions/11534902/ruby-on-rails-app-works-locally-but-does-not-work-on-heroku
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_token
 end
