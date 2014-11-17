055bcfc26f07bd1af9b6e3abe1c30d53ef3e18ed
adrienkalombo@outlook.com <adrienkalombo@outlook.com>
2014-11-17 17:00:36 -0500

    added capybara test for the user controller created more test for tasks, log in and out.

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,1 +1,30 @@
 # Write loging capybara here
+require 'spec_helper'
+
+describe 'user signs up ' do
+  it 'user should go to Welcome to Pomodoro App' do
+
+    within("#session") do
+      fill_in 'Email', :with => 'user@example.com'
+      fill_in 'password', :with => 'password'
+      click_button('sign in')
+    end
+  end
+end
+
+
+describe 'user creates taks' do
+  it 'should take user to task listing' do
+  end
+end
+
+describe ' user log in and out' do
+  it 'should take user to the welcome page' do
+  end
+end
+
+
+describe 'user can use timer' do
+  it ' user can only have timer to 25 minutes'do
+  end
+end
